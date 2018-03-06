5c45a2eae3bc50f895d0fe879c31b44381ca63e9
David Riff <david.riff.94@gmail.com>
2018-03-06 20:15:30 +0100

    minor improvements

--- kangaroo-compare/kangaroo-compare.go
+++ kangaroo-compare/kangaroo-compare.go
@@ -48,7 +48,7 @@ func main(){
 
     	}
 
-    	fmt.Printf("This files have %d differences of a total of %d bytes. It is a %f/100", differences,len(original_file), float32(differences)*100/float32(len(original_file)))
+    	fmt.Printf("This files have %d differences in a total of %d bytes. It is a %f/100", differences,len(original_file), float32(differences)*100/float32(len(original_file)))
 
     }else{
     	fmt.Printf("This files do not have the same size! Original: %d New: %d", len(original_file), len(new_file))
